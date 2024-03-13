5abf987cdf9fcb045434c9f6967b9f40b9d77d15
AKASH P C <iamakashpc@gmail.com>
2024-03-13 19:02:16 +0530

    error handler middleware created

--- server/middlewares/errorMiddleware.js
+++ server/middlewares/errorMiddleware.js
@@ -3,3 +3,22 @@ const notFound = (req,res,next)=>{
     req.status(404);
     next(error);
 }
+
+
+const errorHandler = (err,req,res,next)=>{
+	let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+	let errorMessage = err.message;
+
+	// If Mongoose not found error, set to 404 and change message
+
+    if(err.name ==='CastError'&& err.kind ==='ObjectId'){
+        statusCode=404;
+        message ='Resource not found';
+    }
+    res.status(statusCode).json({
+        message,
+        stack:process.env.NODE_ENV === 'production' ? null : err.stack
+    });
+}
+
+export { notFound , errorHandler};
