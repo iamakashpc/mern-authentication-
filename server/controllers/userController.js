4ed2edb53ebe452431a9a3df49b2422e45189ed5
AKASH P C <iamakashpc@gmail.com>
2024-03-15 13:28:58 +0530

    register with  jwt cookie

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,24 +1,33 @@
-import asyncHandler from "express-async-handler";
 import User from "../model/userModel.js";
-
-// @desc    Register a new user
-// @route   POST /api/users
-// @access  Public
+import generateToken from "../utils/generateToken.js";
+import asyncHandler from "express-async-handler";
 const registerUser = asyncHandler(async (req, res) => {
 	const { name, email, password } = req.body;
 
-	// Check if user already exists
-	const userExist = await User.findOne({ email });
-	if (userExist) {
+	const userExists = await User.findOne({ email });
+
+	if (userExists) {
 		res.status(400);
 		throw new Error("User already exists");
 	}
 
-	// Create new user
-	const user = await User.create({ name, email, password });
+	const user = await User.create({
+		name,
+		email,
+		password,
+	});
 
-	// Send response
-	res.status(201).json(user);
+	if (user) {
+		generateToken(res, user._id); // Assuming generateToken function is properly defined
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+		});
+		// Send token in the Authorization header
+	} else {
+		res.status(400);
+		throw new Error("Invalid user data");
+	}
 });
-
 export { registerUser };
