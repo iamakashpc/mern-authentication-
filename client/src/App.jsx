1aa762276858e0ae8de895e30d9d2b2ce64366ff
AKASH P C <iamakashpc@gmail.com>
2024-03-17 15:19:25 +0530

    Added a Hero component to the Home page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
+import { Container } from "react-bootstrap";
 import Header from "./components/Header"
+import { Outlet } from "react-router-dom";
 
 
 const App = () => {
   return (
-   <>
-    <Header/>
-   </>
-  )
+		<>
+			<Header />
+			<Container className="my-2">
+				<Outlet />
+			</Container>
+		</>
+	);
 }
 
 export default App
