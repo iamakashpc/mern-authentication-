466aef6f040ddab2b0bf69b350586638f95788ce
AKASH P C <iamakashpc@gmail.com>
2024-03-19 11:15:49 +0530

    Header component updated with user login/logout functionality.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,15 @@
-import { Container, Nav, Navbar } from "react-bootstrap"
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
-
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../slices/authSlice";
 const Header = () => {
-  return (
+	const { userInfo } = useSelector((state) => state.auth);
+
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	return (
 		<header>
 			<Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
 				<Container>
@@ -13,21 +19,36 @@ const Header = () => {
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav" />
 					<Nav className="ms-auto">
-						<LinkContainer to="/login">
-							<Nav.Link>
-								<FaSignInAlt /> Sign In
-							</Nav.Link>
-						</LinkContainer>
-						<LinkContainer to="/register">
-							<Nav.Link>
-								<FaSignOutAlt /> Sign Up
-							</Nav.Link>
-						</LinkContainer>
+						{userInfo ? (
+							<>
+								<NavDropdown title={userInfo.name} id="username">
+									<LinkContainer to="/profile">
+										<NavDropdown.Item>Profile</NavDropdown.Item>
+									</LinkContainer>
+									<NavDropdown.Item >
+										Logout
+									</NavDropdown.Item>
+								</NavDropdown>
+							</>
+						) : (
+							<>
+								<LinkContainer to="/login">
+									<Nav.Link>
+										<FaSignInAlt /> Sign In
+									</Nav.Link>
+								</LinkContainer>
+								<LinkContainer to="/register">
+									<Nav.Link>
+										<FaSignOutAlt /> Sign Up
+									</Nav.Link>
+								</LinkContainer>
+							</>
+						)}
 					</Nav>
 				</Container>
 			</Navbar>
 		</header>
 	);
-}
+};
 
-export default Header
+export default Header;
